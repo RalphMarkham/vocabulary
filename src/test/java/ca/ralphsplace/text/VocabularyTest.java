1f60bdfc1c28b3326dc8a2b141dd519098cef321
RalphMarkham <rem@rogers.com>
2020-12-22 15:19:28 -0500

    playing with streams

--- src/test/java/ca/ralphsplace/text/VocabularyTest.java
+++ src/test/java/ca/ralphsplace/text/VocabularyTest.java
@@ -3,7 +3,9 @@ package ca.ralphsplace.text;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -11,9 +13,10 @@ import java.nio.file.Paths;
 class VocabularyTest {
 
     @Test
-    void wordCount() throws URISyntaxException {
+    void wordCount() throws URISyntaxException, IOException {
         Path path = Paths.get(Vocabulary.class.getResource("/t8.shakespeare.txt").toURI());
         Vocabulary vocabulary = new Vocabulary();
-        vocabulary.wordCount(path).subscribe(map -> Assertions.assertTrue(map.get(27643).contains("the")));
+        vocabulary.asyncWordCount(Files.lines(path)).subscribe(map -> Assertions.assertTrue(map.get(27643).contains("the")));
+        Assertions.assertTrue(vocabulary.aggregateMap(vocabulary.wordCount(Files.lines(path))).get(27643L).contains("the"));
     }
 }
